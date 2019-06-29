9c3fc8c4c8d0d42f5f57a9bcaa76d6e4a7d8fe98
dotnetCarpenter <jon.ronnenberg@gmail.com>
2019-06-29 04:01:33 +0200

    simplify fst implementation

--- fst.js
+++ fst.js
@@ -3,9 +3,17 @@
  * If the element is undefined or the array is empty
  * fst will return null.
  * @param {[*]} array
- * @returns {[]|null}
+ * @returns {*|null}
  */
 export default function fst (array) {
-    const first = array.length > 0 ? array[0] : null
-    return first != null ? first : null
+  if (array.length === 0) return null
+  if (array[0] == null) return null
+  return array[0]
 }
+
+// test
+/*console.log(fst([]) === null)
+console.log(fst([,]) === null)
+console.log(fst([0]) === 0)
+console.log(fst([0,1]) === 0)
+*/
