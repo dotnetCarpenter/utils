6d3957400d2ab289374ac3029135c0c44761c8e4
dotnetCarpenter <jon.ronnenberg@gmail.com>
2019-07-01 05:28:22 +0200

    fixed path for readdir and regex pattern syntax

--- testLib/exec.js
+++ testLib/exec.js
@@ -2,22 +2,24 @@ import os from 'os'
 import path from 'path'
 import fs from 'fs'
 import { execFile } from 'child_process'
+import { fileURLToPath } from 'url'
 import config from './testConfig.json'
 import filter from '../filter.js'
 import map from '../map.js'
-import identity from '../identity.js';
+import identity from '../identity.js'
 
 const CPU_COUNT = os.cpus().length
 const testDir = config.spec_dir
 const testFileMatchers = config.spec_files
+const globToRegExp = glob => path.basename(glob).replace(/\./g, '\\.').replace(/\*/g, '.+')
 const getFiles = filter(
   file => map(
-    matcher => new RegExp(path.basename(matcher)).test(file),
+    matcher => new RegExp(globToRegExp(matcher)).test(file),
     testFileMatchers).some(identity)
   )
 
 fs.readdir(
-  path.resolve('..', testDir),
+  path.resolve(path.dirname(fileURLToPath(import.meta.url)), '..', testDir),
   { withFileTypes: true },
   (err, files) => {
     if (err) throw err
