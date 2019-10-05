b5b9076021d9030b936791b6559e6d9cc57b086c
dotnetCarpenter <jon.ronnenberg@gmail.com>
2019-10-05 19:59:28 +0200

    fixed returning first parameter instead of timestamp + documentation

--- log.js
+++ log.js
@@ -1,8 +1,17 @@
+//@ts-check
+'use strict'
+
 const appStartTime = Date.now()
+/**
+ * console.log function that returns the first parameter
+ * and adds a timestamp in front of the messages to print.
+ * @param  {...any} msgs
+ * @returns {any} The first parameter passed to log.
+ */
 export default function log (...msgs) {
   // KLUDGE: the .toString() is because chrome devtools makes number and string
   // mixed in a console.log look super weird.
   msgs.unshift( (Date.now() - appStartTime).toString() )
   console.log(...msgs)
-  return msgs[0]
+  return msgs[1]
 }
