bc923936d75428f241b6a84b0ef54e2ae7df375e
dotnetCarpenter <jon.ronnenberg@gmail.com>
2019-10-06 15:28:42 +0200

    functors are more complicated to use than simple null check

--- bad_API/client.js
+++ bad_API/client.js
@@ -3,6 +3,7 @@
 
 import API from './stubAPI.js'
 import map from '../map.js'
+import { maybe, nothing } from '../functors.js'
 
 main()
 
@@ -14,6 +15,38 @@ main()
  * We solve all of these misbehaves by utilizing functors.
  */
 function main () {
+  let [fChars, fCharCodes] = withFunctor()
+  console.log(fChars, fCharCodes)
+
+  let [chars, charCodes] = withoutFunctor()
+  console.log(chars, charCodes)
+}
+
+function withFunctor () {
+  const message = maybe(API.items())
+
+  if (message instanceof nothing) return ['API.items() returned nothing',]
+
+  let chars
+  message.map(/** @param {string[]} message */ message => {
+    chars = message.join()
+  })
+
+  let charCodes
+  message.map(/** @param {string[]} message */ message => {
+    charCodes = map(char => char.charCodeAt(), message)
+  })
+
+  return [chars, charCodes]
+}
+
+function withoutFunctor () {
   const message = API.items()
-  console.log(message.join(), map(/** @param {string} char */ char => char.charCodeAt(0), message))
+
+  if (!message) return ['API.items() returned nothing',]
+
+  let chars = message.join()
+  let charCodes = map(char => char.charCodeAt(), message)
+
+  return [chars, charCodes]
 }
